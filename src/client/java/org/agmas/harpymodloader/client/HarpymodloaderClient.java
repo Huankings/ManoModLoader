5eb89811503b1499936dfdc298d2f379adac829f
Huankings <hdw15099973617@163.com>
2026-07-15 08:43:42 +0800

    mood hud

--- src/client/java/org/agmas/harpymodloader/client/HarpymodloaderClient.java
+++ src/client/java/org/agmas/harpymodloader/client/HarpymodloaderClient.java
@@ -1,9 +1,11 @@
 package org.agmas.harpymodloader.client;
 
 import dev.doctor4t.wathe.api.Role;
+import dev.doctor4t.wathe.api.client.mood.MoodHudApi;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.util.Identifier;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import java.util.ArrayList;
@@ -20,6 +22,16 @@ public class HarpymodloaderClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HarpyInstinctHandlers.register();
+        /*
+         * Harpy 自己的 modded game mode 本质上仍使用 Wathe 的心情 HUD。
+         * 以前这里靠 MoodRenderer mixin 把 MODDED_GAMEMODE 伪装成 MURDER；
+         * 现在改成正式注册入口，后续其它 game mode 也可以按同样方式接入。
+         */
+        MoodHudApi.registerVisibleGameModePredicate(
+                Identifier.of(Harpymodloader.MOD_ID, "mood/modded_game_mode"),
+                MoodHudApi.DEFAULT_PRIORITY,
+                gameMode -> gameMode == Harpymodloader.MODDED_GAMEMODE
+        );
 
         ClientPlayConnectionEvents.JOIN.register((clientPlayNetworkHandler, packetSender, minecraftClient) -> {
             Harpymodloader.refreshRoles();
